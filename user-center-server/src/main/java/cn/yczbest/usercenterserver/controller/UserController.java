b7212d81162231ee31700c645ff7e4fd31bc3bb8
SmartBest-Zhi <276996733@qq.com>
2025-07-16 21:11:28 +0800

    基本CRUD

--- user-center-server/src/main/java/cn/yczbest/usercenterserver/controller/UserController.java
+++ user-center-server/src/main/java/cn/yczbest/usercenterserver/controller/UserController.java
@@ -3,6 +3,8 @@ package cn.yczbest.usercenterserver.controller;
 import cn.yczbest.galaxy.common.BaseResponse;
 import cn.yczbest.usercenterserver.entity.User;
 import cn.yczbest.usercenterserver.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +30,57 @@ public class UserController {
         return BaseResponse.Success(userService.list(param));
     }
 
+    @PostMapping("/register")
+    public BaseResponse register(@RequestBody User param) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("nick_name", param.getNickName());
+        if (userService.count(queryWrapper) > 0){
+            return BaseResponse.Error("用户已存在");
+        }else{
+            userService.save(param);
+            return BaseResponse.Success("注册成功");
+        }
+    }
+
+    @PostMapping("/delete")
+    public BaseResponse delete(@RequestBody String nickName) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("nick_name", nickName);
+        if (userService.count(queryWrapper) > 0){
+            userService.remove(queryWrapper);
+            return BaseResponse.Success("删除成功");
+        }else{
+            return BaseResponse.Error("用户不存在");
+        }
+    }
+
+    @PostMapping("/update")
+    public BaseResponse update(@RequestBody User param) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("nick_name", param.getNickName());
+        if (userService.count(queryWrapper) > 0){
+            userService.update(queryWrapper);
+            return BaseResponse.Success("修改成功");
+        }else{
+            return BaseResponse.Error("用户不存在");
+        }
+    }
+
+    /**
+     * 获取单个用户信息（注意是Get请求，并且注解变了）
+     * @param nickName
+     * @return
+     */
+    @PostMapping("/info")
+    public BaseResponse info(@RequestParam String nickName) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("nick_name", nickName);
+        User user = userService.getOne(queryWrapper);
+        if (user != null){
+            return BaseResponse.Success(user);
+        }else{
+            return BaseResponse.Error("用户不存在");
+        }
+    }
 
 }
